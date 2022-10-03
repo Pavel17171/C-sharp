324ad9594ce90547378d21d941cb43f8b274b2e2
[1] <[1.asus.rfrf]>
2022-10-03 12:21:15 +0300

    Add Folder_5 if-else

---
+++ New_Folder/Folder_5_if_else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите Ваше имя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "anna")
+{
+    Console.WriteLine("Uraa, Anna!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
